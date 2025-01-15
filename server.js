a4cac8ebe8d7ad88b5a97d5075f99680c7154669
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2025-01-15 01:25:56 -0300

    change again to deploy

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/app/dist/user-manager-web'));
+app.use(express.static(__dirname + '/dist/user-manager-web'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/app/dist/user-manager-web/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/user-manager-web/index.html'));
 });
 
 app.listen(process.env.PORT || 8080)
